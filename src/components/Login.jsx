4fbe52963bfc0f06999997c20076f4275634ec0c
Kehinde Ojapa <k-ojapa@leadway.com>
2022-05-13 00:39:19 +0100

    login page UI done, logic still ongoing devt

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,70 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
+// import GoogleLogin from "react-google-login";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
+// @ts-ignore
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
 const Login = () => {
-  return <div>Login</div>;
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          // @ts-ignore
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="shareme-logo" />
+          </div>
+          <div>
+            {/* <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+              scope="profile"
+            /> */}
+
+            <GoogleLogin
+              onSuccess={(response) => {
+                const result = jwt_decode(response?.credential);
+                console.log(result?.name);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
